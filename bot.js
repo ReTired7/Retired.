460f973fb8b6cbaadd9e27ad71b3455f27bbd61e
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-04 08:27:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,29 +3,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(Logged in as ${client.user.tag}!);
- client.user.setActivity("!help",{type: 'Retired'})
-  
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 client.login('token');
